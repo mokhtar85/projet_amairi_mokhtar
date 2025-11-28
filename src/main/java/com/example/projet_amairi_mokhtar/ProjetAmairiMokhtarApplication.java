2cf878e6972247f8f736bfd01b582dc14e5763b6
mokhtar <mokhtar.amairi@epita.fr>
2025-11-28 09:59:37 +0100

    Initial commit

---
+++ src/main/java/com/example/projet_amairi_mokhtar/ProjetAmairiMokhtarApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_amairi_mokhtar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAmairiMokhtarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetAmairiMokhtarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projet_amairi_mokhtar/ProjetAmairiMokhtarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_amairi_mokhtar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAmairiMokhtarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
